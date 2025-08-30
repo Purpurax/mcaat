b75a9f6bc5d7852959246756ae1d511bc4750fb5
Purpurax <maximer.2006@gmx.de>
2025-08-30 15:19:29 +0200

    Increased accuracy further

--- src/tmp_utils.cpp
+++ src/tmp_utils.cpp
@@ -66,118 +66,8 @@ void print_sdbg_graph_to_dot_file_convert(const string& lib_file_path) {
 }
 
 
-void rotateLeft(std::vector<uint64_t>& arr, int k) {
-    int n = arr.size();
-    if (n == 0) return; // Handle empty array case
-    
-    k = k % n; // In case k is greater than the size of the array
-    if (k == 0) return; // No rotation needed if k is 0 or a multiple of n
-    
-
-    // Step 1: Reverse the first k elements
-    std::reverse(arr.begin(), arr.begin() + k);
-    
-    // Step 2: Reverse the remaining n-k elements
-    std::reverse(arr.begin() + k, arr.end());
-    
-    // Step 3: Reverse the entire array
-    std::reverse(arr.begin(), arr.end());
-}
-
-std::vector<uint64_t> FindRepeatNodePaths(
-    SDBG& sdbg,
-    const vector<uint64_t>& repeat_nodes,
-    const vector<vector<uint64_t>>& ordered_cycles
-) {
-    uint64_t start;
-    uint64_t end;
-    vector<uint64_t> all_the_neighbors;
-    
-    //LIST
-    for(const auto& node : repeat_nodes) {
-        uint64_t outgoings[4]; 
-        int num_outgoings = sdbg.OutgoingEdges(node, outgoings);
-        
-        for(int i = 0; i < num_outgoings; i++) {
-            all_the_neighbors.push_back(outgoings[i]);
-        }
-    }
-
-    for(const auto& node : repeat_nodes) {
-        if(std::find(all_the_neighbors.begin(), all_the_neighbors.end(), node) == all_the_neighbors.end()) {
-            start = node;
-        }
-    }
-        
-    int maxSize = 0;
-    vector<vector<uint64_t>> cycles_per_group = ordered_cycles;
-    std::vector<uint64_t> arr;
-    auto it = std::find(arr.begin(), arr.end(), start);
-    int position_to_rotate = std::distance(arr.begin(), it);
-
-    // Loop through the list of vectors and find the one with the max number of elements
-    for (int i = 0; i < cycles_per_group.size(); i++) {
-        rotateLeft(cycles_per_group[i], position_to_rotate);
-        if (cycles_per_group[i].size() > maxSize) {
-            maxSize = cycles_per_group[i].size();
-            arr = cycles_per_group[i];  // Update the vector with the maximum size
-        }
-    }
-    
-    rotateLeft(arr, position_to_rotate);
-
-    arr.resize(repeat_nodes.size());
-
-    return arr;
-}
-
-pair<vector<uint64_t>, vector<vector<uint64_t>>> FindCRISPRArrayNodes(
-    SDBG& sdbg,
-    const vector<vector<uint64_t>>& ordered_cycles
-) {
-    int threshold = static_cast<int>(ordered_cycles.size());
-    
-    std::unordered_map<uint64_t, int> element_count;
-    for (const auto& cycle : ordered_cycles) {
-        for (const auto& element : cycle) {
-            element_count[element]++;
-        }
-    }
-
-    std::vector<uint64_t> repeat_nodes;
-    for (const auto& [element, count] : element_count) {
-        if (count >= threshold) {
-            repeat_nodes.push_back(element);
-        }
-    }
-
-    if (repeat_nodes.size() >= 27) {
-        return {{}, {}};
-    }
-    
-    std::vector<std::vector<uint64_t>> spacer_nodes;
-    
-    repeat_nodes = FindRepeatNodePaths(sdbg, repeat_nodes, ordered_cycles);
-    
-    for (const auto& cycle : ordered_cycles) {
-        // IMPORTANT: DO NOT ADD UPPER BOUNDARY
-        if (cycle.size() - repeat_nodes.size() >= 23) {
-            std::vector<uint64_t> spacers(cycle.begin() + repeat_nodes.size(), cycle.end());
-            
-            spacer_nodes.push_back(spacers);
-        }
-        // IMPORTANT: DO NOT ADD UPPER BOUNDARY
-    }
-
-    if(repeat_nodes.size() == 0 || spacer_nodes.size() < 3) {
-        return {{}, {}};
-    }
-
-    return {repeat_nodes, spacer_nodes};
-}
-
 string FetchNodeLabel(SDBG& sdbg, const size_t& node) {
-    std::string label;
+    string label;
     uint8_t seq[sdbg.k()];
     uint32_t t = sdbg.GetLabel(node, seq);
     for (int i = sdbg.k() - 1; i >= 0; --i) label.append(1, "ACGT"[seq[i] - 1]);
@@ -185,54 +75,142 @@ string FetchNodeLabel(SDBG& sdbg, const size_t& node) {
     return label;
 }
 
-pair<string, vector<string>> get_systems(
+pair<vector<uint64_t>, string> Find_CRISPR_repeat_nodes_and_sequence(
     SDBG& sdbg,
-    const vector<vector<uint64_t>>& ordered_cycles,
-    int& number_of_spacers
+    const vector<vector<uint64_t>>& ordered_cycles
 ) {
-    auto CRISPRArrayNodes = FindCRISPRArrayNodes(sdbg, ordered_cycles);
-    auto spacers_nodes = CRISPRArrayNodes.second;
-    vector<uint64_t> repeat_nodes = CRISPRArrayNodes.first;
-
-    if (CRISPRArrayNodes.first.empty() || CRISPRArrayNodes.second.empty()) {
-        return std::make_pair(string(), vector<string>());
-    }
-
-    string repeat = FetchNodeLabel(sdbg, repeat_nodes[0]);
+    const int threshold = static_cast<int>(0.9 * static_cast<float>(ordered_cycles.size()) + 0.5);
     
-    for (size_t i = 1; i < repeat_nodes.size(); i++) {
-        std::string node_label = FetchNodeLabel(sdbg, repeat_nodes[i]);
-
-        // Method 1: Using back() method
-        char lastChar = node_label.back();  // Get the last character
-        std::string lastCharStr(1, lastChar);  // Convert char to string
-        
-        repeat += lastCharStr;
+    unordered_map<uint64_t, int> element_count;
+    for (const auto& cycle : ordered_cycles) {
+        for (const auto& element : cycle) {
+            element_count[element]++;
+        }
     }
 
+    // Get all repeat_nodes
+    vector<uint64_t> repeat_nodes;
+    unordered_set<uint64_t> repeat_nodes_set;
+    for (const auto& cycle : ordered_cycles) {
+        bool passed_spacers = false;
+        bool going_through_repeat_region = false;
+        int loop_counter = 0;
+
+        // The spacers might wrap around, therefore go through cycle twice:
+        // Starting with S -> SSSSSRRRRRSSSSS SSSSSRRRRRSSSSS
+        // Starting with R -> RRRRRSSSSSRRRRR RRRRRSSSSSRRRRR
+        while (loop_counter < 2) {
+            for (const auto& node : cycle) {
+                bool is_spacer_node = element_count[node] < threshold;
+                if (is_spacer_node) {
+                    passed_spacers = true;
+                    if (going_through_repeat_region) {
+                        break;
+                    }
+                } else if (passed_spacers) {
+                    going_through_repeat_region = true;
+                    bool already_in_set = repeat_nodes_set.insert(node).second;
+                    if (!already_in_set) {
+                        repeat_nodes.push_back(node);
+                    }
+                }
+            }
+
+            loop_counter++;
+        }
+        for (const auto& [element, count] : element_count) {
+            if (static_cast<float>(count) >= threshold) {
+                repeat_nodes.push_back(element);
+            }
+        }
+    }
+
+    // Choose repeat node sequence that occur the most
+    int repeat_nodes_count = 0;
+    int best_idx = 0;
+    for (int i = 0; i < ordered_cycles.size(); ++i) {
+        const auto cycle = ordered_cycles.at(i);
+
+        int current_repeat_nodes_count = 0;
+        for (const auto& node : cycle) {
+            bool is_repeat = std::find(repeat_nodes.begin(), repeat_nodes.end(), node) != repeat_nodes.end();
+            if (is_repeat) {
+                current_repeat_nodes_count += element_count[node];
+            }
+        }
+
+        if (current_repeat_nodes_count > repeat_nodes_count) {
+            repeat_nodes_count = current_repeat_nodes_count;
+            best_idx = i;
+        }
+    }
+
+    // Fetch best sequence of repeat nodes
+    string repeat = "";
+    bool passed_spacers = false;
+    bool going_through_repeat_region = false;
+    int loop_counter = 0;
+
+    // The spacers might wrap around, therefore go through cycle twice:
+    // Starting with S -> SSSSSRRRRRSSSSS SSSSSRRRRRSSSSS
+    // Starting with R -> RRRRRSSSSSRRRRR RRRRRSSSSSRRRRR
+    while (loop_counter < 2) {
+        for (const auto& node : ordered_cycles.at(best_idx)) {
+            bool is_repeat_node = std::find(repeat_nodes.begin(), repeat_nodes.end(), node) != repeat_nodes.end();
+            if (!is_repeat_node) {
+                passed_spacers = true;
+                if (going_through_repeat_region) {
+                    // Idk why, but one letter is missing
+                    char new_char = FetchNodeLabel(sdbg, node).back();
+                    string new_char_str(1, new_char);
+                    repeat += new_char_str;
+                    break;
+                }
+            } else if (passed_spacers) {
+                going_through_repeat_region = true;
+                if (repeat == "") {
+                    repeat = FetchNodeLabel(sdbg, node);
+                } else {
+                    char new_char = FetchNodeLabel(sdbg, node).back();
+                    string new_char_str(1, new_char);
+                    repeat += new_char_str;
+                }
+            }
+        }
+
+        loop_counter++;
+    }
+
+    return {repeat_nodes, repeat};
+}
+
+tuple<string, vector<string>, string> get_systems(
+    SDBG& sdbg,
+    const vector<vector<uint64_t>>& ordered_cycles
+) {
+    auto [repeat_nodes, repeat] = Find_CRISPR_repeat_nodes_and_sequence(sdbg, ordered_cycles);
     
     vector<string> spacers;
     for (const auto& cycle : ordered_cycles) {
         string spacer = "";
 
         bool passed_repeat = false;
-        bool passed_spacers = false;
+        bool going_through_spacer_region = false;
         int loop_counter = 0;
 
-        // The spacers might wrap around, therefore go through cycle twice
+        // The spacers might wrap around, therefore go through cycle twice:
+        // Starting with S -> SSSSSRRRRRSSSSS SSSSSRRRRRSSSSS
+        // Starting with R -> RRRRRSSSSSRRRRR RRRRRSSSSSRRRRR
         while (loop_counter < 2) {
             for (const auto& node : cycle) {
                 bool is_repeat_node = std::find(repeat_nodes.begin(), repeat_nodes.end(), node) != repeat_nodes.end();
                 if (is_repeat_node) {
                     passed_repeat = true;
-                    if (passed_spacers) {
+                    if (going_through_spacer_region) {
                         break;
                     }
-                } else {
-                    passed_spacers = true;
-                }
-
-                if (!is_repeat_node && passed_repeat) {
+                } else if (passed_repeat) {
+                    going_through_spacer_region = true;
                     if (spacer == "") {
                         spacer = FetchNodeLabel(sdbg, node);
                     } else {
@@ -242,6 +220,7 @@ pair<string, vector<string>> get_systems(
                     }
                 }
             }
+
             loop_counter++;
         }
 
@@ -253,10 +232,11 @@ pair<string, vector<string>> get_systems(
         spacers.push_back(spacer);
     }
     
-    // if(spacers.size() < 3){
-        //     return std::make_pair(string(), vector<string>());
-        // }
-        
-    number_of_spacers = spacers.size();
-    return std::make_pair(repeat, spacers);
+    string crispr_sequence = repeat; // roughly R-S1-R-S2-...-R-SN-R
+    for (auto spacer_it = spacers.rbegin(); spacer_it != spacers.rend(); ++spacer_it) {
+        crispr_sequence += *spacer_it;
+        crispr_sequence += repeat;
+    }
+
+    return std::make_tuple(repeat, spacers, crispr_sequence);
 }
